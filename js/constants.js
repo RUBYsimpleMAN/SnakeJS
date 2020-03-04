66da717c89cc15ee46a63bcd22326af8f6a272a8
UtopiaEH <utopiaeh01@gmail.com>
2020-03-05 00:17:41 +0200

    First commit

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('game');
+export const ctx = canvas.getContext('2d');
+
+export const scoreText = document.getElementById('score');
+
+export const BOX = 32;
+export const ARROW_DOWN = 'ArrowDown';
+export const ARROW_UP = 'ArrowUp';
+export const ARROW_LEFT = 'ArrowLeft';
+export const ARROW_RIGHT = 'ArrowRight';
